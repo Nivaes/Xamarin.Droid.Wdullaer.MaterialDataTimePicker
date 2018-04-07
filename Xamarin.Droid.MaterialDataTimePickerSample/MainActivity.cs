0f61db20fe8a483419c3657ff62c1593b2e01b6d
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2018-04-07 22:45:37 +0200

    Packaje update.

--- Xamarin.Droid.MaterialDataTimePickerSample/MainActivity.cs
+++ Xamarin.Droid.MaterialDataTimePickerSample/MainActivity.cs
@@ -1,27 +1,79 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
 
 namespace Nivaes.MaterialDataTimePicker.Droid
 {
-    [Activity(Label = "AndroidMaterialDataTimePickerSample", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    [Activity(Label = "@string/app_name", 
+        MainLauncher = true, 
+        Theme = "@style/AppTheme",
+        Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : AppCompatActivity
     {
-        int count = 1;
+        ViewPager viewPager;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            //base.SetContentView(R.layout.activity_main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            //adapter = new PickerAdapter(getFragmentManager());
+            //viewPager = findViewById(R.id.pager);
+            //viewPager.setAdapter(adapter);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+            //TabLayout tabLayout = findViewById(R.id.tabs);
+            //tabLayout.setupWithViewPager(viewPager);
+            //for (int i = 0; i < adapter.getCount(); i++) //noinspection ConstantConditions
+            //    tabLayout.getTabAt(i).setText(adapter.getTitle(i));
         }
+
+        //private class PickerAdapter extends FragmentPagerAdapter
+        //{
+        //private static final int NUM_PAGES = 2;
+        //Fragment timePickerFragment;
+        //Fragment datePickerFragment;
+
+        //PickerAdapter(FragmentManager fm)
+        //{
+        //    super(fm);
+        //    timePickerFragment = new TimePickerFragment();
+        //    datePickerFragment = new DatePickerFragment();
+        //}
+
+        //@Override
+        //public int getCount()
+        //{
+        //    return NUM_PAGES;
+        //}
+
+        //@Override
+        //public Fragment getItem(int position)
+        //{
+        //    switch (position)
+        //    {
+        //        case 0:
+        //            return timePickerFragment;
+        //        case 1:
+        //        default:
+        //            return datePickerFragment;
+        //    }
+        //}
+
+        //int getTitle(int position)
+        //{
+        //    switch (position)
+        //    {
+        //        case 0:
+        //            return R.string.tab_title_time;
+        //        case 1:
+        //        default:
+        //            return R.string.tab_title_date;
+        //    }
+        //}
     }
 }
+
 
