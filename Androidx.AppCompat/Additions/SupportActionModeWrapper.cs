5b20a7e34066a09d8b54947bf11b92b2721573f5
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2019-05-26 23:18:17 +0200

    Compila.

--- Androidx.AppCompat/Additions/SupportActionModeWrapper.cs
+++ Androidx.AppCompat/Additions/SupportActionModeWrapper.cs
@@ -1,21 +1,6 @@
 ﻿namespace AndroidX.AppCompat.View
 {
-    using Java.Lang;
-
     public partial class SupportActionModeWrapper
     {
-        //public override ICharSequence SubtitleFormatted
-        //{
-        //    set => SetSubtitle
-        //    set => SetSubtitleFormatted(value);
-        //}
-
-        //public override ICharSequence TitleFormatted { set => SetTitleFormatted(value); }
-
-        //public override ICharSequence SubtitleFormatted
-        //{
-        //    get => null;
-        //    set => void;
-        //}
     }
 }
