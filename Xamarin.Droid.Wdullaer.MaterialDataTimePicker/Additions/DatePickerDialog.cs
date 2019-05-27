17f25dfbf4eae2d4aff25b2a2eb5f022ff0bab07
agimenez <andres.gimenez@outlook.com>
2019-05-27 10:31:45 +0200

    Petardazo

--- Xamarin.Droid.Wdullaer.MaterialDataTimePicker/Additions/DatePickerDialog.cs
+++ Xamarin.Droid.Wdullaer.MaterialDataTimePicker/Additions/DatePickerDialog.cs
@@ -1,19 +1,17 @@
 ﻿namespace Com.Wdullaer.MaterialDateTimePicker.Date
 {
-    using Android.Support.V7.App;
-    using Android.Support.V7.Widget;
     using Java.Util;
     using static Android.Views.View;
 
-    public partial class DatePickerDialog
-    {
-        public DatePickerDialog(DatePickerDialog.IOnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth)
-        {
-            Initialize(callBack, year, monthOfYear, dayOfMonth);
-        }
+    //public partial class DatePickerDialog
+    //{
+    //    //public DatePickerDialog(DatePickerDialog.IOnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth)
+    //    //{
+    //    //    Initialize(callBack, year, monthOfYear, dayOfMonth);
+    //    //}
 
-        //DatePickerDialog.ScrollOrientation IDatePickerController.ScrollOrientation => GetScrollOrientation();
+    //    //DatePickerDialog.ScrollOrientation IDatePickerController.ScrollOrientation => GetScrollOrientation();
 
-        //DatePickerDialog.Version IDatePickerController.Version => GetVersion();
-    }
+    //    //DatePickerDialog.Version IDatePickerController.Version => GetVersion();
+    //}
 }
